099b1c1d590290a13db723cbc487b6b3c4cebf58
Ehsan-Abedin <abedinehsan1380@gmail.com>
2020-06-20 17:20:45 +0430

    Test

--- src/main.java
+++ src/main.java
@@ -4,12 +4,5 @@ public class main {
         System.out.println("Hi Fucking Project!");
         System.out.println("Ehsan");
         System.out.println("Ali");
-        //rgikegrelkkheth
-        tehikethlrth
-                ergleirgoemg
-                erniperignerg
-                        drgnoeirg
-
-
     }
 }
