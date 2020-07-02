44d9138052ad493b7d1dfb5c490be20df6c287a6
Ehsan-Abedin <abedinehsan1380@gmail.com>
2020-07-02 20:28:22 +0430

    Test

--- src/CurrentSource.java
+++
@@ -1,5 +0,0 @@
-public class CurrentSource extends Element {
-    public CurrentSource(float current, float voltage, float power, String node1, String node2) {
-        super(current, voltage, power, node1, node2);
-    }
-}
---
+++ src/VoltageSourceDC.java
@@ -0,0 +1,5 @@
+public class VoltageSourceDC extends Element {
+    public VoltageSourceDC(float current, float voltage, float power, String node1, String node2, String name) {
+        super(current, voltage, power, node1, node2, name);
+    }
+}
