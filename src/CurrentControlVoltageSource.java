492fd4da8f1e3a924d9e744d91d8845c4c601099
Ehsan-Abedin <abedinehsan1380@gmail.com>
2020-07-02 20:39:28 +0430

    Test

--- src/CurrentControlVoltageSource.java
+++ src/CurrentControlVoltageSource.java
@@ -1,10 +1,12 @@
 public class CurrentControlVoltageSource extends Element {
-    String controlNode1, ControlNode2;
+    String controlNode1, controlNode2;
+    float gain;
 
-    public CurrentControlVoltageSource(float current, float voltage, float power, String node1, String node2, String name, String controlNode1, String controlNode2) {
+    public CurrentControlVoltageSource(float current, float voltage, float power, String node1, String node2, String name, String controlNode1, String controlNode2, float gain) {
         super(current, voltage, power, node1, node2, name);
         this.controlNode1 = controlNode1;
-        ControlNode2 = controlNode2;
+        this.controlNode2 = controlNode2;
+        this.gain = gain;
     }
 
     public String getControlNode1() {
@@ -12,7 +14,7 @@ public class CurrentControlVoltageSource extends Element {
     }
 
     public String getControlNode2() {
-        return ControlNode2;
+        return controlNode2;
     }
 
     public void setControlNode1(String controlNode1) {
@@ -20,6 +22,6 @@ public class CurrentControlVoltageSource extends Element {
     }
 
     public void setControlNode2(String controlNode2) {
-        ControlNode2 = controlNode2;
+        this.controlNode2 = controlNode2;
     }
 }
