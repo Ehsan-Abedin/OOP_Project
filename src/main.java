67a7cd3761419d557c339c22bea909459a021f6c
Ehsan-Abedin <abedinehsan1380@gmail.com>
2020-07-24 19:47:37 +0430

    Test

--- src/main.java
+++ src/main.java
@@ -3,37 +3,52 @@ public class main {
         if ((Capacitor.getAllCapacitors() == null) && (Inductance.getAllInductances() == null)) {
             int n = Node.getAllNodes().size();
             int m = VoltageSourceDC.getAllVoltageSourceDCs().size() + VoltageControlVoltageSource.getAllVoltageControlVoltageSources().size() + CurrentControlVoltageSource.getAllCurrentControlVoltageSources().size();
-            double a[][] = new double[m+n+1][m+n+1];
-            double g[][] = new double[n+1][n+1];
-            double b[][] = new double[n+1][m+1];
-            double c[][] = new double[m+1][n+1];
-            double d[][] = new double[m+1][m+1];
-            for (int i = 1; i < n+1; i++) {
-                for (int j = 1; j < n+1; j++){
+            double a[][] = new double[m + n + 1][m + n + 1];
+            double g[][] = new double[n + 1][n + 1];
+            double b[][] = new double[n + 1][m + 1];
+            double c[][] = new double[m + 1][n + 1];
+            double d[][] = new double[m + 1][m + 1];
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
                     double sum = 0;
                     for (Resistance allResistance : Resistance.getAllResistances()) {
-                        if (i == j)
+                        if (i == j) {
                             if ((allResistance.getNode1() == i) || (allResistance.getNode2() == i))
                                 sum += 1 / allResistance.getResistance();
-                        else
-                            if (((allResistance.getNode1() == i) && (allResistance.getNode2()) == j) || ((allResistance.getNode1() == j) && (allResistance.getNode2()) == i))
+                            else if (((allResistance.getNode1() == i) && (allResistance.getNode2()) == j) || ((allResistance.getNode1() == j) && (allResistance.getNode2()) == i))
                                 sum -= 1 / allResistance.getResistance();
+                        }
+                        else {
+
+                        }
+                    }
+                    for (VoltageControlCurrentSource allVoltageControlCurrentSource : VoltageControlCurrentSource.getAllVoltageControlCurrentSources()) {
+                        if (allVoltageControlCurrentSource.getNode1() == i) {
+                            if (allVoltageControlCurrentSource.getControlNode1() == j)
+                                sum += allVoltageControlCurrentSource.getGain();
+                            else if (allVoltageControlCurrentSource.getControlNode2() == j)
+                                sum -= allVoltageControlCurrentSource.getGain();
+                        }
+                        else if (allVoltageControlCurrentSource.getNode2() == 2) {
+                            if (allVoltageControlCurrentSource.getControlNode1() == j)
+                                sum -= allVoltageControlCurrentSource.getGain();
+                            else if (allVoltageControlCurrentSource.getControlNode2() == j)
+                                sum += allVoltageControlCurrentSource.getGain();
+                        }
                     }
                     g[i][j] = sum;
                 }
             }
-            for (int i = 1; i < n+1; i++){
-                for (int j = 1; j < m+1; j++){
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < m + 1; j++) {
                     for (VoltageSourceDC allVoltageSourceDc : VoltageSourceDC.getAllVoltageSourceDCs()) {
                         if (allVoltageSourceDc.getNode1() == i) {
                             b[i][j] = 1;
                             c[j][i] = 1;
-                        }
-                        else if (allVoltageSourceDc.getNode2() == i) {
+                        } else if (allVoltageSourceDc.getNode2() == i) {
                             b[i][j] = -1;
                             c[j][i] = -1;
-                        }
-                        else {
+                        } else {
                             b[i][j] = 0;
                             c[j][i] = 0;
                         }
@@ -48,8 +63,7 @@ public class main {
                             else
                                 c[j][i] = 1;
 
-                        }
-                        else if (allVoltageControlVoltageSource.getNode2() == i) {
+                        } else if (allVoltageControlVoltageSource.getNode2() == i) {
                             b[i][j] = -1;
                             if (allVoltageControlVoltageSource.getControlNode1() == i)
                                 c[j][i] = -allVoltageControlVoltageSource.getGain() - 1;
@@ -57,8 +71,7 @@ public class main {
                                 c[j][i] = allVoltageControlVoltageSource.getGain() - 1;
                             else
                                 c[j][i] = -1;
-                        }
-                        else {
+                        } else {
                             b[i][j] = 0;
                             if (allVoltageControlVoltageSource.getControlNode1() == i)
                                 c[j][i] = -allVoltageControlVoltageSource.getGain();
@@ -68,9 +81,23 @@ public class main {
                                 c[j][i] = 0;
                         }
                     }
+                    for (CurrentControlVoltageSource allCurrentControlVoltageSource : CurrentControlVoltageSource.getAllCurrentControlVoltageSources()) {
+                        if (allCurrentControlVoltageSource.getNode1() == i) {
+                            b[i][j] = 1;
+                            c[j][i] = 1;
+                        }
+                        else if (allCurrentControlVoltageSource.getNode2() == i) {
+                            b[i][j] = -1;
+                            c[j][i] = -1;
+                        }
+                        else {
+                            b[i][j] = 0;
+                            c[j][i] = 0;
+                        }
+                    }
                 }
             }
-
         }
     }
 }
+
